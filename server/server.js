c18aafc50e3cff03c34645a653054580c5cedc77
Da1man <chaos4ever@mail.ru>
2020-06-09 00:29:39 +0300

    add server

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const PORT = 3000;
+const DOCROOT = './../dist/';
+
+
+//  1. Подключить нужные модули http, path, express
+
+const http = require('http');
+const path = require('path');
+const express = require('express');
+
+
+//  2. Создать сервер, используя express и http
+
+const app = express();
+const server = http.createServer(app);
+
+
+// 3. Настроить отдачу игры при запросе к серверу
+
+const documentRoot = path.join(__dirname, DOCROOT);
+const staticContent = express.static(documentRoot);
+app.use(staticContent);
+
+
+// 4.Запускаем сервер
+
+server.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`)
+});
